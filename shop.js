b90bc498e21b8f1cec721fa49b6ae5f090e0fd2f
Nothing-28 <sumanrj475@gmail.com>
2024-07-31 17:42:16 +0530

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // $(".down1").click(function(){
+    //     $(".dropdown-menu").slideToggle();
+    // })
+    // $(".down2").click(function(){
+    //     $(".dropdown-menu1").slideToggle();
+    // })
+    $(".hamberger").click(function(){
+        $(".nav-navigation-item").toggleClass("menu");
+    })
+})
